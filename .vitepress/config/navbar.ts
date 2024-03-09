5c42ddad4a827932afc210a3bc1ea69a04eeccad
niemingzhi <1094290505@qq.com>
2024-03-09 22:18:40 +0800

    feat: 增加订单超时自动取消方案

--- .vitepress/config/navbar.ts
+++ .vitepress/config/navbar.ts
@@ -1,60 +1,60 @@
-import { NavItem } from 'vitepress/types/default-theme'
+import { NavItem } from "vitepress/types/default-theme";
 
 export const nav: NavItem[] = [
   {
-    text: '主页',
-    link: '/',
+    text: "主页",
+    link: "/",
   },
   {
-      text:'Java整理',
-      activeMatch: '^/java/',
-      link: '/java/线程池相关.md',
+    text: "Java整理",
+    activeMatch: "^/java/",
+    link: "/java/线程池相关.md",
   },
   {
-    text: '算法整理',
-    activeMatch: '^/leetcode/',
-    link: '/leetcode/目录.md',
+    text: "算法整理",
+    activeMatch: "^/leetcode/",
+    link: "/leetcode/menu.md",
   },
   {
-    text: 'Rust整理',
-    activeMatch: '^/rust/',
-    link: '/rust/rust环境安装.md',
+    text: "Rust整理",
+    activeMatch: "^/rust/",
+    link: "/rust/rust环境安装.md",
   },
   {
-    text: '文章整理',
-    activeMatch: '^/article/',
-    link: '/article/文章整理.md',
+    text: "文章整理",
+    activeMatch: "^/article/",
+    link: "/article/文章整理.md",
   },
   {
-    text: '常用工具类Utils整理',
-    activeMatch: '^/utils/',
-    link: '/utils/计算工具类ArithUtils.md',
+    text: "常用工具类Utils整理",
+    activeMatch: "^/utils/",
+    link: "/utils/计算工具类ArithUtils.md",
   },
   {
-    text: '杂项',
-    activeMatch: '^/snippets/',
+    text: "杂项",
+    activeMatch: "^/snippets/",
     items: [
       {
-        text: '常用软件',
-        link: '/snippets/常用软件.md',
+        text: "常用软件",
+        link: "/snippets/常用软件.md",
       },
       {
-        text: 'Chrome插件',
-        link: '/snippets/Chrome浏览器插件.md',
+        text: "Chrome插件",
+        link: "/snippets/Chrome浏览器插件.md",
       },
       {
-        text: '油猴插件',
-        link: '/snippets/油猴脚本.md',
+        text: "油猴插件",
+        link: "/snippets/油猴脚本.md",
       },
       {
-        text: '双拼输入法',
-        link: '/snippets/设置双拼输入法.md',
+        text: "双拼输入法",
+        link: "/snippets/设置双拼输入法.md",
       },
       {
-        text: 'Idea整理',
-        activeMatch: '^/idea/',
-        link: '/idea/Idea的liveTemplates整理.md',
+        text: "Idea整理",
+        activeMatch: "^/idea/",
+        link: "/idea/Idea的liveTemplates整理.md",
       },
     ],
   },
-]
+];
